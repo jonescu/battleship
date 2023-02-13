974a81cda3a07ee826cfb95ec12ba3cb160add79
Nick Jonescu <nicholasjonescu@gmail.com>
2023-02-13 17:31:10 -0500

    writing factory functions

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
+// Select the grid and all game pieces
 const grid = document.querySelector('.grid');
+const gamePieces = document.querySelectorAll('.game-piece');
 
+// Create the grid
 for (let i = 0; i < 100; i++) {
-  const cell = document.createElement('div');
-  grid.appendChild(cell);
+  const cell = document.createElement('div')
+  grid.appendChild(cell)
 }
 
-grid.addEventListener('click', (e) => {
-  // handle user interactions here
-});
+function createShip(length, numOfHits, isSunk) {
+  return {
+    length, 
+    numOfHits, 
+    isSunk,
+    hit() {
+      numOfHits++
+    },
+    isSunk() {
+      if(this.length === this.numOfHits) {
+        this.isSunk = true
+      }
+    }
+  }
+}
+
+// // Variables to keep track of the current game piece being dragged and its orientation
+// let currentPiece;
+// let currentOrientation = 'horizontal';
+
+// // Allow game pieces to be dropped onto the grid
+// grid.addEventListener('dragover', (e) => {
+//   e.preventDefault();
+// });
+
+// // Handle the drag start event for game pieces
+// gamePieces.forEach(gamePiece => {
+//   gamePiece.addEventListener('dragstart', (e) => {
+//     currentPiece = e.target;
+//     currentOrientation = 'horizontal'; // or 'vertical'
+//     e.dataTransfer.setData('text/plain', currentPiece.id);
+//   });
+// });
+
+// // Handle the drop event for game pieces onto the grid
+// grid.addEventListener('drop', (e) => {
+//   e.preventDefault();
+  
+//   // Get the id of the game piece being dropped
+//   const pieceId = e.dataTransfer.getData('text/plain');
+//   const piece = document.querySelector(`#${pieceId}`);
+//   const pieceSize = piece.dataset.size;
+
+//   // Check if the placement of the game piece is valid
+//   const cell = e.target;
+  
+//   // Place the game piece on the board
+//   cells.forEach(c => c.appendChild(piece));
+// });
