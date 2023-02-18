80bb24bab2781afc177ef961d80478a17f9714b1
Nick Jonescu <nicholasjonescu@gmail.com>
2023-02-18 14:31:42 -0500

    added enemy grid, drag and drop needs work

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // import {gameBoard, createShip} from './factories.js';
 // Select the grid and all game pieces
-const player_grid = document.querySelector('#grid')
+const player_grid = document.querySelector('#player-grid')
+const enemy_grid = document.querySelector('#enemy-grid')
 const ships_container = document.querySelector('#game-pieces')
 const player_ships = document.querySelectorAll('.game-piece')
 // Create the grid
@@ -10,12 +11,17 @@ for (let i = 0; i < 100; i++) {
   player_grid.appendChild(cell)
 }
 
+for(let i = 0; i< 100; i++) {
+  const enemy_cell = document.createElement('div')
+  enemy_cell.classList.add('enemy-cell')
+  enemy_grid.appendChild(enemy_cell)
+}
+
 const grid_cells = player_grid.querySelectorAll('.grid-cell')
 
 // Implement drag & drop functionality
 player_ships.forEach(ship => {
   ship.addEventListener('dragstart', dragStart)
-  ship.addEventListener('dragenter', dragEnter)
 })
 
 grid_cells.forEach(cell => {
@@ -30,11 +36,6 @@ function dragStart(e) {
   e.dataTransfer.setData('text/plain', JSON.stringify({pieceId, subPieceIds}))
 }
 
-function dragEnter(e) {
-  e.preventDefault()
-  // console.log('enter')
-}
-
 function dragOver(e) {
   e.preventDefault();
   // console.log('over')
@@ -44,20 +45,16 @@ function drop(e) {
   e.preventDefault();
   const {pieceId, subPieceIds} = JSON.parse(e.dataTransfer.getData('text/plain'))
   const gamePiece = document.getElementById(pieceId)
-  const gameBoardCellSize = 1
   const gamePieceSize = subPieceIds.length
-  const cellsToOccupy = gamePieceSize 
   const dropTarget = e.target.closest('.grid-cell')
   // Check that the drop target has enough cells to accommodate the game piece
   const cells = Array.from(dropTarget.parentElement.querySelectorAll('.grid-cell'))
-  // const cellsInRow = cells.length / 10
   const cellIndex = Array.from(cells).indexOf(dropTarget)
-  const cellsToCheck = cells.slice(cellIndex, cellIndex + cellsToOccupy)
-  console.log(cellsToCheck)
+  const cellsToCheck = cells.slice(cellIndex, cellIndex + gamePieceSize)
   if (cellsToCheck.every(cell => !cell.classList.contains('occupied'))) {
     // Occupy the cells with the game piece and its sub-pieces
     dropTarget.classList.add('occupied')
-    for (let i = 0; i < cellsToOccupy; i++) {
+    for (let i = 0; i < gamePieceSize; i++) {
       cells[cellIndex + i].classList.add('occupied')
     }
     gamePiece.style.display = 'none'
