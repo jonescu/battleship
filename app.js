53e4161845f9a50dd4333f43092a7d85e4b50b70
Nick Jonescu <nicholasjonescu@gmail.com>
2023-02-22 16:41:03 -0500

    working on rotating pieces

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const enemy_grid = document.querySelector('#enemy-grid')
 const ships_container = document.querySelector('#game-pieces')
 const player_ships = document.querySelectorAll('.game-piece')
 
-// Create the grid
+// Create the player grid
 for (let row = 0; row < 10; row++) {
   for (let col = 0; col < 10; col++) {
     // Create a new div element for the grid cell
@@ -15,14 +15,13 @@ for (let row = 0; row < 10; row++) {
       // Set a unique identifier for the edge pieces
       cell.id = `edge-${row}-${col}`;
     }
-    
     // Add the cell to the container
     player_grid.appendChild(cell)
   }
 }
 
 // Create enemy grid
-for(let i = 0; i< 100; i++) {
+for (let i = 0; i < 100; i++) {
   const enemy_cell = document.createElement('div')
   enemy_cell.classList.add('enemy-cell')
   enemy_cell.id = `${i}`
@@ -47,7 +46,7 @@ function dragStart(e) {
   // Store the IDs of the game piece and its sub-pieces in the dataTransfer object
   const pieceId = e.target.id
   const subPieceIds = Array.from(e.target.querySelectorAll('.sub-piece')).map(subPiece => subPiece.id)
-  e.dataTransfer.setData('text/plain', JSON.stringify({pieceId, subPieceIds}))
+  e.dataTransfer.setData('text/plain', JSON.stringify({ pieceId, subPieceIds }))
 }
 
 function dragOver(e) {
@@ -56,7 +55,7 @@ function dragOver(e) {
 
 function drop(e) {
   e.preventDefault()
-  const {pieceId, subPieceIds} = JSON.parse(e.dataTransfer.getData('text/plain'))
+  const { pieceId, subPieceIds } = JSON.parse(e.dataTransfer.getData('text/plain'))
   const gamePiece = document.getElementById(pieceId)
   const gamePieceSize = subPieceIds.length
   const dropTarget = e.target.closest('.grid-cell')
@@ -66,24 +65,29 @@ function drop(e) {
   const cellIndex = Array.from(cells).indexOf(dropTarget)
 
   // Prevent pieces from being placed directly next to one another
-  let cellsToCheck = cells.slice(cellIndex -1, cellIndex + gamePieceSize + 1)
+  let cellsToCheck = cells.slice(cellIndex - 1, cellIndex + gamePieceSize + 1)
   const rowsToCheck = []
 
   // Allows user to place piece on left side of board without piece wrapping to line above
-  if(+cellIndex.toString().slice(1) === 0) {
+  if (+cellIndex.toString().slice(1) === 0) {
     cellsToCheck = cells.slice(cellIndex, cellIndex + gamePieceSize + 1)
   }
 
-  // console.log(cellIndex.toString().slice(1))
-  for (let i = 0; i < cellsToCheck.length -1; i++) {
-    if(cellsToCheck[i].id != '') {
-      rowsToCheck.push(+cellsToCheck[i].id.slice(5,6))
+  for (let i = 0; i < cellsToCheck.length - 1; i++) {
+    if (cellsToCheck[i].id != '') {
+      rowsToCheck.push(+cellsToCheck[i].id.slice(5, 6))
     }
+  }
+
+  // Allows user to place piece on left side of board when a piece occupies the furthest right cell one row above
+  if (+cellsToCheck.at(-1).id.slice(5,6) !== cellsToCheck.at(-2).id.slice(5,6) &&
+  cellsToCheck.at(-1).classList.contains('occupied')) {
+    cellsToCheck.pop()
   }
   
   // Check if cells aren't occupied, or on different rows
-  if (cellsToCheck.every(cell => !cell.classList.contains('occupied')) && 
-      rowsToCheck.every(row => row === rowsToCheck[0])) {
+  if (cellsToCheck.every(cell => !cell.classList.contains('occupied')) &&
+    rowsToCheck.every(row => row === rowsToCheck[0])) {
     // Occupy the cells with the game piece and its sub-pieces
     dropTarget.classList.add('occupied')
     for (let i = 0; i < gamePieceSize; i++) {
